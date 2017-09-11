d9ca4a20851de353a8e7c7b775a7c7e7958065c9
freyssin <andre.freyssinet@scalagent.com>
2017-09-11 15:54:07 +0200

    New module for WS2812 Wemos shield.

---
+++ src/WS2812.lua
@@ -0,0 +1,81 @@
+-- LGPL v3 License (Free Software Foundation)
+-- Copyright (C) 2017 ScalAgent Distributed Technologies
+
+-- WS2812 module
+-- NOTE: D4 (GPIO2) pin is used by the library, but shield uses D2 (GPIO4)
+
+local WS2812 = {}
+
+local dev, publish
+
+-- WS2812 shield parameters
+
+local delay=1000
+local lighton = false
+
+red     = {0,90,0}
+green   = {90,0,0}
+blue    = {0,0,90}
+yellow  = {45,45,0}
+magenta = {0,45,45}
+cyan    = {45,0,45}
+white   = {30,30,30}
+off     = {0,0,0}
+
+color = green
+
+-- Declare component functions below
+
+local function set_color(msg)
+  if (msg = 
+end
+
+local function toggle_ws2812()
+  print("toggle_ws2812")
+  if lighton == true then
+    lighton = false
+    ws2812.write(string.char(0,0,0))
+  else
+    lighton = true
+    ws2812.write(string.char(color[1],color[2],color[3]))
+  end
+end
+
+local function blink_ws2812()
+  toggle_ws2812()
+  tmr.create():alarm(delay, tmr.ALARM_SINGLE, toggle_ws2812)
+end
+
+local function off_ws2812()
+  ws2812.write(string.char(0,0,0))
+end
+
+-- Initialisation function
+local function init_ws2812(d, p)
+  dev = d
+  publish = p
+  -- Initialisation of the WS2812 shield
+  ws2812.init(ws2812.MODE_SINGLE)
+  ws2812.write(string.char(0,0,0))
+end
+
+-- Table of functions 
+local actions = {
+  ["init"] = init_ws2812,
+  ["toggle"] = toggle_ws2812,
+  ["blink"] = blink_ws2812,
+  ["off"] = off_ws2812
+}
+
+-- These 2 methods are needed by micro-service framework
+WS2812.init = init_ws2812
+WS2812.actions = actions
+-- These methods below are only needed for external use of the WS2812 module
+WS2812.toggle = toggle_ws2812
+WS2812.blink = blink_ws2812
+WS2812.off = off_ws2812
+
+init_ws2812()
+blink_ws2812()
+
+return WS2812
