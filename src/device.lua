bd3e0d8b92a917eeb3d2f52503c2af1c66006d18
freyssin <andre.freyssinet@scalagent.com>
2017-09-11 15:49:03 +0200

    minor changes.

--- src/device.lua
+++ src/device.lua
@@ -11,14 +11,26 @@ local dev, publish
 
 -- Declare module functions below
 
-local function get_info(msg)
+local function get_info()
   local major, minor, v, chipid, flashid = node.info()
   local msg = major.."."..minor..","..v..","..chipid..","..flashid
   publish(dev, "info", msg)
 end
 
-local function get_fsinfo(msg)
+local function get_fsinfo()
   publish(dev, "fsinfo", file.fsinfo())
+end
+
+local function get_heap()
+  publish(dev, "heap", node.heap())
+end
+
+local function get_flashsize()
+  publish(dev, "flashsize", node.flashsize())
+end
+
+local function restart()
+  node.restart()
 end
 
 -- Initialisation function
@@ -31,14 +43,20 @@ end
 local actions = {
   ["init"] = init_dev,
   ["get_info"] = get_info,
-  ["get_fsinfo"] = get_fsinfo
+  ["get_fsinfo"] = get_fsinfo,
+  ["get_heap"] = get_heap,
+  ["get_flashsize"] = get_flashsize,
+  ["restart"] = restart
 }
 
 -- These 2 methods are needed by micro-service framework
 DEV.init = init_dev
 DEV.actions = actions
--- These methods are only needed for external use of the LED module
+-- These methods are only needed for external use of the device module
 DEV.get_info = get_info
 DEV.get_fsinfo = get_fsinfo
+DEV.get_heap = get_heap
+DEV.get_flashsize = get_flashsize
+DEV.restart = restart
 
 return DEV
