6d4d5773f0e32d93d1570b29b52c44e25865482c
freyssin <andre.freyssinet@scalagent.com>
2018-09-04 09:10:09 +0200

    adss "on", "off" and "set" functions.

--- src/relay.lua
+++ src/relay.lua
@@ -1,7 +1,10 @@
 -- LGPL v3 License (Free Software Foundation)
--- Copyright (C) 2017 ScalAgent Distributed Technologies
+-- Copyright (C) 2017 - 2018 ScalAgent Distributed Technologies
 
 -- Relay module
+
+-- Normally the relay use the pin 4 (GPIO2) and is not compatible with I2C
+-- devices. This component corresponds to a modified shield using pin 5.
 
 local RELAY = {}
 
@@ -9,13 +12,31 @@ local dev, publish
 
 -- Relay shield parameters
 
-local gpio_pin = 5  -- GPIO 2 Blue led
+local gpio_pin = 5
 local gpio_state = gpio.LOW
 local delay = 1000
 
 -- Declare module functions below
 
-function toggle()
+function on_relay()
+  gpio_state = gpio.HIGH
+  gpio.write(gpio_pin, gpio_state)
+end
+
+function off_relay()
+  gpio_state = gpio.LOW
+  gpio.write(gpio_pin, gpio_state)
+end
+
+local function set_relay(msg)
+  if msg == "true" then
+  	on_relay()
+  else
+  	off_relay()
+  end
+end
+
+function toggle_relay()
   if gpio_state == gpio.LOW then
     gpio_state = gpio.HIGH
   else
@@ -24,9 +45,9 @@ function toggle()
   gpio.write(gpio_pin, gpio_state)
 end
 
-function blink()
-  toggle()
-  tmr.create():alarm(delay, tmr.ALARM_SINGLE, toggle)
+function blink_relay()
+  toggle_relay()
+  tmr.create():alarm(delay, tmr.ALARM_SINGLE, toggle_relay)
 end
 
 -- Initialisation function
@@ -40,15 +61,19 @@ end
 
 -- Table of functions 
 local actions = {
-  ["toggle"] = toggle,
-  ["blink"] = blink
+  ["toggle"] = toggle_relay,
+  ["blink"] = blink_relay,
+  ["on"] = on_relay,
+  ["off"] = off_relay,
+  ["set"] = set_relay
 }
 
 -- These 2 methods are needed by micro-service framework
 RELAY.init = init
 RELAY.actions = actions
 -- These methods are only needed for external use of the relay module
-RELAY.toggle = toggle
-RELAY.blink = blink
+RELAY.toggle = toggle_relay
+RELAY.blink = blink_relay
+RELAY.set = set_relay
 
 return RELAY
